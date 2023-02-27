356ba4337edbb191343e1d63ff505ae0c5f38827
stefanalexandruoanea <125955018+stefanalexandruoanea@users.noreply.github.com>
2023-02-27 23:50:41 +0200

    Add files via upload

--- docker/mypython.py
+++ docker/mypython.py
@@ -1,5 +1,10 @@
-f = open("demofile.txt", "r")
-for x in f :
-    if x!="." or x!="," or x!="! " or x!="?" or x!="a"or x!="e"or x!="i" or x!="o" or x!="u"
-        print(x)
-f.close()
+import pandas as pd
+import string
+def remove_punctuation(text):
+    translator = str.maketrans('', '', string.punctuation)
+    return text.translate(translator)
+
+df = pd.read_csv('my_file.txt', sep='\t', header=None, names=['Text'])
+df['Text'] = df['Text'].apply(remove_punctuation)
+
+print(df)
